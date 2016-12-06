274fd6b401e93055d36a7a3d5a635f0663d17edf
nitish <nitish@NITISH-PC>
2016-12-07 04:48:20 +0530

    Billing Scaffolded

---
+++ MedicalBilling/Controllers/BillingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicalBilling.Controllers
+{
+    public class BillingController : Controller
+    {
+        private BillingEntities db = new BillingEntities();
+        // GET: Billing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View()
+        {
+            return View();
+        }
+    }
+}
---
+++ MedicalBilling/ViewModel/BillingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalBilling.ViewModel
+{
+    public class BillingViewModel
+    {
+        public int id { get; set; }
+        public Nullable<int> cid { get; set; }
+        public Nullable<int> uid { get; set; }
+        public Nullable<System.DateTime> date { get; set; }
+        public Nullable<System.DateTime> dueDate { get; set; }
+        public Nullable<int> status { get; set; }
+
+        public List<InvoiceItem> invoiceItem { get; set; }
+    }
+}
